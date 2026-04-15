c459a1c002f6d0b197643baec3815bd48467fb84
princessnwedo <nwedoprincess@gmail.com>
2026-04-15 11:43:00 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,37 +1,102 @@
 #include <stdio.h>
+#include <unistd.h>
 
-int main(){
     typedef enum{
         RED,
         RED_AMBER,
         GREEN,
         AMBER
     }State;
-    
+
+void printState(State currentState);
+int getDelay(State currentState);
+State updateState(State currentState);
+
+int main(){
    State currentState = RED;
+   int pedestrianRequest = 0;
+   char userInput = 'n';
     
-    for(int i=1;i<=8;i++){
-    switch (currentState){
-        case RED:
-        printf("RED\n");
-        currentState = RED_AMBER;
-        break;
+    for(int i=1;i<=10;i++){
+    printState(currentState);
         
-        case RED_AMBER:
-        printf("RED_AMBER\n");
-        currentState = GREEN;
-        break;
-        
-        case GREEN:
-        printf("GREEN\n");
-        currentState = AMBER;
-        break;
-        
-        case AMBER:
-        printf("AMBER\n");
-        currentState = RED;
-        break;
+    if(currentState == GREEN){    
+    printf("Activate pedestrian button?: \n");
+    scanf(" %c", &userInput);
+    
+    if(userInput == 'y'){
+    pedestrianRequest = 1;
+    printf("Pedestrian Access Requested\n");
     }
+    }
+    
+    sleep(getDelay(currentState));
+    
+    if(pedestrianRequest == 1 && currentState == RED){
+    printf("Walk\n");
+    sleep(5);
+    pedestrianRequest = 0;
+    }
+    
+    currentState = updateState(currentState);
     }
     return 0;
+}
+
+void printState(State currentState){
+    switch(currentState){
+        case RED:
+            printf("RED\n");
+            break;
+
+        case RED_AMBER:
+            printf("RED_AMBER\n");
+            break;
+        
+        case GREEN:
+            printf("GREEN\n");
+            break;
+        
+        case AMBER:
+            printf("AMBER\n");
+            break;
+    }
+}
+
+int getDelay(State currentState){
+    switch(currentState){
+        case RED:
+        return 5;
+        
+        case RED_AMBER:
+        return 1;
+        
+        case GREEN:
+        return 5;
+        
+        case AMBER:
+        return 2;
+    }
+    return 0;
+}
+
+State updateState(State currentState){
+    switch(currentState){
+        case RED:
+            currentState = RED_AMBER;
+            break;
+
+        case RED_AMBER:
+            currentState = GREEN;
+            break;
+        
+        case GREEN:
+            currentState = AMBER;
+            break;
+        
+        case AMBER:
+            currentState = RED;
+            break;
+    }
+    return currentState;
 }
